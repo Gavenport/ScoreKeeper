63ac35c91a3c96c5d937533238c79637642cddf9
Gavenport <bernd.ungerer@googlemail.com>
2018-03-12 09:36:45 +0100

    All works fine now so this is the final version for this challenge.

--- app/src/main/java/com/example/gavenport/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/gavenport/scorekeeper/MainActivity.java
@@ -14,25 +14,65 @@ public class MainActivity extends AppCompatActivity {
     int cornerTeamA = 0;
     int cornerTeamB = 0;
 
+    // Saves the scores of both teams in case of changing activity
+    static final String STATE_SCORE_A = "scoreTeamA";
+    static final String STATE_SCORE_B = "scoreTeamB";
+    static final String STATE_PENALTY_A = "penaltyKickTeamA";
+    static final String STATE_PENALTY_B = "penaltyKickTeamB";
+    static final String STATE_CORNER_A = "cornerTeamA";
+    static final String STATE_CORNER_B = "cornerTeamB";
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putInt(STATE_SCORE_A, scoreTeamA);
+        savedInstanceState.putInt(STATE_SCORE_B, scoreTeamB);
+        savedInstanceState.putInt(STATE_PENALTY_A, penaltyKickTeamA);
+        savedInstanceState.putInt(STATE_PENALTY_B, penaltyKickTeamB);
+        savedInstanceState.putInt(STATE_CORNER_A, cornerTeamA);
+        savedInstanceState.putInt(STATE_CORNER_B, cornerTeamB);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore state members from saved instance
+        scoreTeamA = savedInstanceState.getInt(STATE_SCORE_A);
+        scoreTeamB = savedInstanceState.getInt(STATE_SCORE_B);
+        penaltyKickTeamA = savedInstanceState.getInt(STATE_PENALTY_A);
+        penaltyKickTeamB = savedInstanceState.getInt(STATE_PENALTY_B);
+        cornerTeamA = savedInstanceState.getInt(STATE_CORNER_A);
+        cornerTeamB = savedInstanceState.getInt(STATE_CORNER_B);
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+        displayCornerForTeamA(cornerTeamA);
+        displayCornerForTeamB(cornerTeamB);
+        displayPenaltyKickForTeamA(penaltyKickTeamA);
+        displayPenaltyKickForTeamB(penaltyKickTeamB);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
-        displayForTeamB(0);
-        displayCornerForTeamA(0);
-        displayCornerForTeamB(0);
-        displayPenaltyKickForTeamA(0);
-        displayPenaltyKickForTeamB(0);
+
+        TextView scoreTeamA = findViewById(R.id.team_a_score);
+        TextView cornerTeamA = findViewById(R.id.team_a_corner);
+        TextView penaltyKickTeamA = findViewById(R.id.team_a_penalty_kick);
+        TextView scoreTeamB = findViewById(R.id.team_b_score);
+        TextView cornerTeamB = findViewById(R.id.team_b_corner);
+        TextView penaltyKickTeamB = findViewById(R.id.team_b_penalty_kick);
+
     }
 
 
 
-
-    /**
-     * Increase penalty kicks for Team A by 1 point.
-     */
-    public void addPenaltyKickForTeamA(View v) {
+        /**
+         * Increase penalty kicks for Team A by 1 point.
+         */
+    public void addPenaltyKickForTeamA(View view) {
 
         penaltyKickTeamA=penaltyKickTeamA+1;
         displayPenaltyKickForTeamA(penaltyKickTeamA);
@@ -41,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Increase corner kicks for Team A by 1 points.
      */
-    public void addCornerForTeamA(View v) {
+    public void addCornerForTeamA(View view) {
 
         cornerTeamA=cornerTeamA+1;
         displayCornerForTeamA(cornerTeamA);
@@ -50,13 +90,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Increase the score for Team A by 1 points.
      */
-    public void addGoalForTeamA(View v) {
+    public void addGoalForTeamA(View view) {
 
         scoreTeamA=scoreTeamA+1;
         displayForTeamA(scoreTeamA);
     }
 
-    public void resetScore(View v) {
+    public void resetScore(View view) {
         scoreTeamA=0;
         scoreTeamB=0;
         penaltyKickTeamA=0;
@@ -70,6 +110,36 @@ public class MainActivity extends AppCompatActivity {
         displayPenaltyKickForTeamA(penaltyKickTeamA);
         displayPenaltyKickForTeamB(penaltyKickTeamB);
     }
+
+
+
+    /**
+     * Increase penalty kicks for Team B by 1 point.
+     */
+    public void addPenaltyKickForTeamB(View view) {
+
+        penaltyKickTeamB=penaltyKickTeamB+1;
+        displayPenaltyKickForTeamB(penaltyKickTeamB);
+    }
+
+    /**
+     * Increase corner kicks for Team B by 1 points.
+     */
+    public void addCornerForTeamB(View view) {
+
+        cornerTeamB=cornerTeamB+1;
+        displayCornerForTeamB(cornerTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 1 points.
+     */
+    public void addGoalForTeamB(View view) {
+
+        scoreTeamB=scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+    }
+
 
 
     /**
@@ -91,37 +161,6 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = findViewById(R.id.team_a_penalty_kick);
         scoreView.setText("Elfmeter: " + String.valueOf(score));
     }
-
-
-
-
-    /**
-     * Increase penalty kicks for Team B by 1 point.
-     */
-    public void addPenaltyKickForTeamB(View v) {
-
-        penaltyKickTeamB=penaltyKickTeamB+1;
-        displayPenaltyKickForTeamB(penaltyKickTeamB);
-    }
-
-    /**
-     * Increase corner kicks for Team B by 1 points.
-     */
-    public void addCornerForTeamB(View v) {
-
-        cornerTeamB=cornerTeamB+1;
-        displayCornerForTeamB(cornerTeamB);
-    }
-
-    /**
-     * Increase the score for Team B by 1 points.
-     */
-    public void addGoalForTeamB(View v) {
-
-        scoreTeamB=scoreTeamB+1;
-        displayForTeamB(scoreTeamB);
-    }
-
     /**
      * Displays the given score for Team B.
      */
@@ -141,4 +180,5 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText("Elfmeter: " + String.valueOf(score));
 
     }
+
 }
